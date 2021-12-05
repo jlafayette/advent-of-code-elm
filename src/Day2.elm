e2e0b2f8c119945783f14ca01ed8084f8fb165e7
Josh LaFayette <joshlafayette@gmail.com>
2021-12-05 02:00:58 -0800

    Solve day2

--- src/Day2.elm
+++ src/Day2.elm
@@ -1,7 +1,6 @@
 module Day2 exposing (view)
 
--- import Day1Input exposing (exampleInput, input)
-
+import Day2Input exposing (exampleInput, input)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
@@ -9,20 +8,139 @@ import Element.Font as Font
 import Element.Input as Input
 
 
+type Cmd
+    = Forward Int
+    | Down Int
+    | Up Int
+
+
+parse : String -> List Cmd
+parse lines =
+    lines
+        |> String.trim
+        |> String.lines
+        |> List.map (\x -> String.trim x)
+        |> List.map parseLine
+
+
+parseLine : String -> Cmd
+parseLine line =
+    let
+        parts =
+            String.words line
+
+        default =
+            Forward 0
+    in
+    case parts of
+        [ commandStr, intStr ] ->
+            let
+                amount =
+                    Maybe.withDefault 0 (String.toInt intStr)
+            in
+            case commandStr of
+                "forward" ->
+                    Forward amount
+
+                "down" ->
+                    Down amount
+
+                "up" ->
+                    Up amount
+
+                _ ->
+                    default
+
+        _ ->
+            default
+
+
+f1 : Cmd -> ( Int, Int ) -> ( Int, Int )
+f1 cmd ( forward, depth ) =
+    case cmd of
+        Forward amount ->
+            ( forward + amount, depth )
+
+        Down amount ->
+            ( forward, depth + amount )
+
+        Up amount ->
+            ( forward, depth - amount )
+
+
+mult : ( Int, Int ) -> Int
+mult ( x, y ) =
+    x * y
+
+
+
+-- expect 150
+
+
 solveExample1 =
-    0
-
-
-solveExample2 =
-    0
+    exampleInput
+        |> parse
+        |> List.foldl f1 ( 0, 0 )
+        |> mult
 
 
 solvePart1 =
-    0
+    input
+        |> parse
+        |> List.foldl f1 ( 0, 0 )
+        |> mult
+
+
+
+---- Part 2 ----
+
+
+type alias SubState =
+    { horizontalPos : Int
+    , aim : Int
+    , depth : Int
+    }
+
+
+f2 : Cmd -> SubState -> SubState
+f2 cmd { horizontalPos, aim, depth } =
+    case cmd of
+        Forward amount ->
+            { horizontalPos = horizontalPos + amount
+            , aim = aim
+            , depth = depth + (aim * amount)
+            }
+
+        Down amount ->
+            { horizontalPos = horizontalPos
+            , aim = aim + amount
+            , depth = depth
+            }
+
+        Up amount ->
+            { horizontalPos = horizontalPos
+            , aim = aim - amount
+            , depth = depth
+            }
+
+
+mult2 : SubState -> Int
+mult2 { horizontalPos, aim, depth } =
+    horizontalPos * depth
+
+
+solveExample2 =
+    exampleInput
+        |> parse
+        |> List.foldl f2 { horizontalPos = 0, aim = 0, depth = 0 }
+        |> mult2
 
 
 solvePart2 =
-    0
+    input
+        |> parse
+        |> List.foldl f2 { horizontalPos = 0, aim = 0, depth = 0 }
+        |> mult2
 
 
 view : Element msg
